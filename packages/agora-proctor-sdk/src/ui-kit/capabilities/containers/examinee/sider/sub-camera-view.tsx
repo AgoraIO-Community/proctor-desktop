016d3409a75f35bd4a81611146ab150bf132daf8
zhanheng <zhanheng@agora.io>
2022-10-10 14:30:07 +0800

    fix: student stream

--- packages/agora-proctor-sdk/src/ui-kit/capabilities/containers/examinee/sider/sub-camera-view.tsx
+++ packages/agora-proctor-sdk/src/ui-kit/capabilities/containers/examinee/sider/sub-camera-view.tsx
@@ -1,6 +1,10 @@
-import { AgoraCard } from "@/ui-kit/components/card";
-import { SupervisorView } from "@/ui-kit/components/supervisor-view";
-
+import { DeviceTypeEnum } from '@/infra/api';
+import { useStore } from '@/infra/hooks/ui-store';
+import { AgoraCard } from '@/ui-kit/components/card';
+import { SupervisorView } from '@/ui-kit/components/supervisor-view';
+import { EduClassroomConfig } from 'agora-edu-core';
+import { AgoraRteVideoSourceType } from 'agora-rte-sdk';
+import { RemoteTrackPlayer } from '../../common/stream/track-player';
 export const SubCameraView = () => {
   return (
     <AgoraCard>
@@ -10,5 +14,29 @@ export const SubCameraView = () => {
 };
 
 const SubCamera = () => {
-  return <div style={{ height: "133px" }}>sub camera</div>;
+  const { userUuid } = EduClassroomConfig.shared.sessionInfo;
+
+  const {
+    roomUIStore: { roomSceneByRoomUuid },
+    usersUIStore: { generateDeviceUuid, generateGroupUuid },
+  } = useStore();
+  const userUuidPrefix = userUuid.split('-')[0];
+
+  const roomUuid = generateGroupUuid(userUuidPrefix)!;
+  const scene = roomSceneByRoomUuid(roomUuid);
+  const subDeviceUserUuid = generateDeviceUuid(userUuidPrefix, DeviceTypeEnum.Sub);
+  const subDeviceStreamUuid = Array.from(
+    scene?.streamController?.streamByUserUuid.get(subDeviceUserUuid) || [],
+  ).find(
+    (streamUuid) =>
+      scene?.streamController?.streamByStreamUuid.get(streamUuid)?.videoSourceType ===
+      AgoraRteVideoSourceType.Camera,
+  );
+  const subDeviceStream =
+    subDeviceStreamUuid && scene?.streamController?.streamByStreamUuid?.get(subDeviceStreamUuid);
+  return (
+    <div style={{ height: '133px' }}>
+      {subDeviceStream && <RemoteTrackPlayer stream={subDeviceStream} />}
+    </div>
+  );
 };
